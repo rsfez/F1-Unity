ea4713af02a203a4c1abd10d16d6b4e1772fadba
Rob <rob@Niseko.local>
2024-05-07 20:02:56 +0900

    Update position text

--- Assets/Scripts/DriverStandingController.cs
+++ Assets/Scripts/DriverStandingController.cs
@@ -4,22 +4,13 @@ using UnityEngine.EventSystems;
 
 public class DriverStandingController : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField]
-    public Color32 colorPrimary;
-    [SerializeField]
-    public Color32 colorSecondary;
-    public Driver driver;
-    private TextMeshProUGUI text;
-    private StandingsController standingsController;
+    [SerializeField] public Color32 colorPrimary;
 
-    public static GameObject CreateGameObject(Transform root, Driver driver)
-    {
-        var driverStanding = Instantiate(Resources.Load("Prefabs/DriverStanding") as GameObject);
-        driverStanding.GetComponent<TextMeshProUGUI>().text = driver.abbreviation;
-        driverStanding.transform.SetParent(root);
-        driverStanding.GetComponent<DriverStandingController>().SetDriver(driver);
-        return driverStanding;
-    }
+    [SerializeField] public Color32 colorSecondary;
+
+    public Driver driver;
+    private StandingsController standingsController;
+    private TextMeshProUGUI text;
 
     private void Awake()
     {
@@ -27,9 +18,23 @@ public class DriverStandingController : MonoBehaviour, IPointerClickHandler
         standingsController = GameObject.FindGameObjectWithTag("Standings").GetComponent<StandingsController>();
     }
 
+    private void Update()
+    {
+        text.text = GetDisplayedText();
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         standingsController.SetDriverSelected(this);
+    }
+
+    public static GameObject CreateGameObject(Transform root, Driver driver)
+    {
+        var driverStanding = Instantiate(Resources.Load("Prefabs/DriverStanding") as GameObject);
+        // driverStanding.GetComponent<TextMeshProUGUI>().text = ;
+        driverStanding.transform.SetParent(root);
+        driverStanding.GetComponent<DriverStandingController>().SetDriver(driver);
+        return driverStanding;
     }
 
     public void OnDriverSelected(bool selected)
@@ -40,11 +45,16 @@ public class DriverStandingController : MonoBehaviour, IPointerClickHandler
     private void SetDriver(Driver driver)
     {
         this.driver = driver;
-        name = driver.position + ". " + driver.abbreviation;
+        name = GetDisplayedText();
     }
 
     public short GetDriverPosition()
     {
         return driver.position;
+    }
+
+    private string GetDisplayedText()
+    {
+        return driver.position + ". " + driver.abbreviation;
     }
 }
